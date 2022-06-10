e9edae3ffd2a34b2f3c1765b2a4f54f4c6459263
fitriadwiseptiani <fitriadwiseptiani11@gmail.com>
2022-06-10 08:33:24 +0700

    update config

--- config/database.js
+++ config/database.js
@@ -1,34 +1,54 @@
+require("dotenv").config();
+
 const {
-  DB_USER= 'cllspqyswwbmdk',
-  DB_PASSWORD='8309ffc4129e58b37a9a233382e21e839bd230b10a7b241322a8aff09de81f4e',
-  DB_NAME= 'd3h6g6hk79h4cn',
-  DB_HOST= 'ec2-3-234-131-8.compute-1.amazonaws.com',
-  DB_PORT= '5432'
+  DB_USER = process.env.DB_USER,
+  DB_PASSWORD = process.env.DB_PASSWORD,
+  DB_NAME = process.env.DB_NAME,
+  DB_HOST = process.env.DB_HOST,
+  DB_PORT = process.env.DB_PORT,
 } = process.env;
 
 module.exports = {
   development: {
     username: DB_USER,
     password: DB_PASSWORD,
-    database: `${DB_NAME}_development`,
+    database: DB_NAME,
     host: DB_HOST,
     port: DB_PORT,
-    dialect: "postgres"
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
   },
   test: {
     username: DB_USER,
     password: DB_PASSWORD,
-    database: `${DB_NAME}_development`,
+    database: DB_NAME,
     host: DB_HOST,
     port: DB_PORT,
-    dialect: "postgres"
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
   },
   production: {
     username: DB_USER,
     password: DB_PASSWORD,
-    database: `${DB_NAME}_production`,
+    database: DB_NAME,
     host: DB_HOST,
     port: DB_PORT,
-    dialect: "postgres"
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
   }
 }
--- jest.config.js
+++ jest.config.js
@@ -33,6 +33,7 @@ module.exports = {
   // Indicates which provider should be used to instrument code for coverage
   coverageProvider: "v8",
 
+  testTimeout: 20000
   // A list of reporter names that Jest uses when writing coverage reports
   // coverageReporters: [
   //   "json",
